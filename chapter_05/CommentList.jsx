4733845a53f81ff1cc417d5e955dfe790c048300
kjh0012024 <109588008+kjh0012024@users.noreply.github.com>
2022-10-02 20:20:26 +0900

    Add files via upload

---
+++ chapter_05/CommentList.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Comment from "./Comment";
+
+const comments= [
+    {
+        name: "김지환",
+        comment: "안녕하세요, 소통해요.",
+    },
+    {
+        name: "유재석",
+        comment: "리액트 재밌었어요~!",
+    },
+    {
+        name: "강민경",
+        comment: "저도 리액트 배워보고 싶어요!!",
+    },
+];
+
+function CommentList(props){
+    return(
+        <div>
+            {comments.map((comment) => {
+                return (
+                    <Comment name={comment.name} comment={comment.comment}/>
+                );
+            })}
+        </div>
+    );
+}
+
+export default CommentList;
